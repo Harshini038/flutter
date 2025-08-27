c6ee95b35c598c6971986eded1f25b6e4f45c696
Harshini038 <141545251+Harshini038@users.noreply.github.com>
2025-08-27 13:20:25 +0530

    Create main.dart

---
+++ contact-list/main.dart
@@ -0,0 +1,326 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Contact- List"),
+          backgroundColor: Colors.lightBlueAccent,
+          centerTitle: false,
+        ),
+        body: ListView(
+          children: [
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 5),
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Sachu",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("brother",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 5)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Achu",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("sister",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 1)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Acha",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("father",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 5)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Amma",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("mother",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+
+
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 5)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Maami",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("brother",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+
+
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 5)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Sachu",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("brother",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 5)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Sachu",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("brother",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 1)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Sachu",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("brother",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            ),
+
+            Container(
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              
+              
+              decoration: BoxDecoration(
+                
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: Offset(0, 1)
+                  )
+                ]
+                ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Sachu",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Text("brother",style: TextStyle(fontSize:14)),
+                Text("56549498786",style: TextStyle(fontSize: 13))
+              ],)
+              
+              
+
+            )
+
+
+          ]
+          ,
+        ),
+      ),
+    );
+  }
+}
